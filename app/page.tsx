9693acd9aeedfea27d58b105ac619c938d7a6635
ZxStim <thang.victor.luong@gmail.com>
2025-05-10 08:51:13 +0700

    fix: social link

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
           asChild
           className="flex items-center p-6 w-full"
         >
-          <Link target="_blank" href="https://x.com/intern_uwu">
+          <Link target="_blank" href="https://x.com/internxyz_">
             <span className="mr-1 text-lg">𝕏</span>
             <span className="text-lg md:text-xl">Follow us</span>
           </Link>
